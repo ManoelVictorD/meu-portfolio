5193f0f983feec1e670acc1378296dc41596d00f
Manoel Victor <victorduarterabelo@live.com>
2023-02-03 10:52:38 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed = new typed("span",{
+    strings:["Manoel Victor Duarte.","Web Developer.","Front-end Developer.","Acadêmico de Ciência da Computação."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
